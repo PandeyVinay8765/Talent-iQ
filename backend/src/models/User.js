05e04ca25a821841dcdac7654f6ffca9d340ced9
Vinay Pandey <pandeyjivinay8765@gmail.com>
2026-02-28 01:43:33 +0530

    fixed

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -14,7 +14,7 @@ const UserSchema=new mongoose.Schema({
         type:String,
         default:"",
     },
-    clarkId:{
+    clerkId:{
         type:String,
         required:true,
         unique:true,
