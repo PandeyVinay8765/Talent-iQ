9dcaa8e589f99783a4e0c3d3223b0dd1577fb625
Vinay Pandey <pandeyjivinay8765@gmail.com>
2026-02-28 00:49:02 +0530

    import issue fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import cors from 'cors';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
-import {serve,functions}from 'inngest';
+import { serve ,functions} from 'inngest/express';
 
 const app=express();
 app.use(express.json());
