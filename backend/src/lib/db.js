adab4b85937f772c990c5c8900c04f51eff0b586
Vinay Pandey <pandeyjivinay8765@gmail.com>
2026-03-08 00:27:59 +0530

    component Updated in Frontend

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,14 +1,19 @@
-import mongoose from 'mongoose'
-import {ENV} from './env.js'
-export const connectDB=async()=>{
-    try{
-        if(!ENV.DB_URL){
-            throw new Error("DB_URL is not defined in .env");
-        }
-        const conn=await mongoose.connect(ENV.DB_URL);
-        console.log("connected to MongoDb:",conn.connection.host)
-    } catch{
-        console.error("Error During connection to mongoDb contact developer :Vinay Pandey")
-        process.exit(1);
+import mongoose from "mongoose";
+
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB_URL is not defined in environment variables");
     }
-}
+
+    const conn = await mongoose.connect(ENV.DB_URL);
+
+    console.log("✅ Connected to MongoDB:", conn.connection.host);
+
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB", error);
+    process.exit(1); // 0 = success, 1 = failure
+  }
+};
