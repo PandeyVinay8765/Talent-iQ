7049c0582426d4375391607fd28f772b2523f612
Vinay Pandey <pandeyjivinay8765@gmail.com>
2026-03-08 01:44:16 +0530

    frontend Ui issue Fixed

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,4 +1,5 @@
 import { StreamChat } from "stream-chat";
+import { StreamClient } from "@stream-io/node-sdk";
 import { ENV } from "./env.js";
 
 const apiKey = ENV.STREAM_API_KEY;
@@ -8,10 +9,9 @@ if (!apiKey || !apiSecret) {
   console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
-// Create Stream chat client
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
+export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
-// Create or update a Stream user
 export const upsertStreamUser = async (userData) => {
   try {
     await chatClient.upsertUser(userData);
@@ -21,12 +21,11 @@ export const upsertStreamUser = async (userData) => {
   }
 };
 
-// Delete a Stream user
 export const deleteStreamUser = async (userId) => {
   try {
     await chatClient.deleteUser(userId);
     console.log("Stream user deleted successfully:", userId);
   } catch (error) {
-    console.error("Error deleting Stream user:", error);
+    console.error("Error deleting the Stream user:", error);
   }
 };
